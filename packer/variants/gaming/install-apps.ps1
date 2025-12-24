bf7fb537df74acd6e0d1a8784b83458fb6a92f72
Connor Edwards <38229097+cedws@users.noreply.github.com>
2025-12-24 22:16:16 +0000

    Fix winget install in gaming VM build

--- packer/variants/gaming/install-apps.ps1
+++ packer/variants/gaming/install-apps.ps1
@@ -7,15 +7,26 @@ if (-not (Test-Path "C:\packer")) {
 Start-Transcript -Path "C:\packer\install-apps.log" -Append
 
 Write-Host "Installing applications via winget..."
-winget install --silent --accept-source-agreements --accept-package-agreements `
-    Microsoft.DotNet.DesktopRuntime.8 `
-    Proton.ProtonVPN `
-    Proton.ProtonPass `
-    Hawaii_Beach.TinyNvidiaUpdateChecker `
-    Valve.Steam `
-    Discord.Discord `
-    Mozilla.Firefox `
-    EpicGames.EpicGamesLauncher
+
+# Install each package individually with --source winget to avoid msstore SSL errors
+$packages = @(
+    "Microsoft.DotNet.DesktopRuntime.8",
+    "Proton.ProtonVPN",
+    "Proton.ProtonPass",
+    "Hawaii_Beach.TinyNvidiaUpdateChecker",
+    "Valve.Steam",
+    "Discord.Discord",
+    "Mozilla.Firefox",
+    "EpicGames.EpicGamesLauncher"
+)
+
+foreach ($package in $packages) {
+    Write-Host "Installing $package..."
+    winget install --id $package --source winget --silent --accept-source-agreements --accept-package-agreements
+    if ($LASTEXITCODE -ne 0) {
+        Write-Warning "Failed to install $package (exit code: $LASTEXITCODE)"
+    }
+}
 
 Write-Host "Applications installed"
 
